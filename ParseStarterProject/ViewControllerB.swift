4f4fbfcebf0b6165adc84353319a8b30c0058886
矢吹祐真 <YumaYabuki@yabukiyuushin-no-MacBook-Air.local>
2015-03-03 23:42:47 +0900

    storyboardつなげた

---
+++ ParseStarterProject/ViewControllerB.swift
@@ -0,0 +1,13 @@
+//
+//  ViewControllerB.swift
+//  ParseStarterProject
+//
+//  Created by 矢吹祐真 on 2015/03/03.
+//  Copyright (c) 2015年 Parse. All rights reserved.
+//
+
+import Cocoa
+
+class ViewControllerB: UIViewController {
+
+}
---
+++ ParseStarterProject/ViewControllerC.swift
@@ -0,0 +1,13 @@
+//
+//  ViewControllerC.swift
+//  ParseStarterProject
+//
+//  Created by 矢吹祐真 on 2015/03/03.
+//  Copyright (c) 2015年 Parse. All rights reserved.
+//
+
+import Cocoa
+
+class ViewControllerC: UIViewController {
+
+}
